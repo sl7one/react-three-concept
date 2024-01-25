ebfa5a7b1f9c364b44a770d0b0c1225630e1de5b
Slava <110696118+sl7one@users.noreply.github.com>
2024-01-25 17:21:36 +0200

    add responsive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,17 @@ import { Canvas } from "@react-three/fiber";
 import Scene from "./Scene";
 import { Suspense } from "react";
 import Front from "./Front";
+import { Container } from "./Container";
 
 export default function App() {
   return (
-    <Suspense fallback={<>LOADING...</>}>
-      <Canvas>
-        <Scene />
-      </Canvas>
-      <Front />
-    </Suspense>
+    <Container>
+      <Suspense fallback={<>LOADING...</>}>
+        <Canvas>
+          <Scene />
+        </Canvas>
+        <Front />
+      </Suspense>
+    </Container>
   );
 }
